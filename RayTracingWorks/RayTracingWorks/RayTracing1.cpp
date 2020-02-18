a85350f94ef239c9318ea280cedd9e62c4d42046
Nicholas Juniper <njuniper99@gmail.com>
2020-02-18 18:54:04 -0500

    Segmented the raytracer to speed shit up. Next up is threading it. Materials, Focus, etc all implemented

--- RayTracingWorks/RayTracingWorks/RayTracing1.cpp
+++ RayTracingWorks/RayTracingWorks/RayTracing1.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <float.h>
+#include <vector>
+#include <thread>
 
 #include "Camera.h"
 #include "Shapes.h"
@@ -13,51 +15,32 @@
 using namespace std;
 using namespace cimg_library;
 
-int max_depth = 10;
+int max_depth = 5;
 int max_samples = 32;
 
-Vec3 color(const Ray& r, Shape* world, int depth=0) {
+Vec3 color(const Ray& r, Shape* world, int depth = 0) {
 	Hit_Record rec;
 	if (world->hit(r, 0.001, FLT_MAX, rec)) {
 		Ray scattered;
-		Vec3 attentuation;
-		if (depth < max_depth && rec.mat_ptr->scatter(r, rec, attentuation, scattered))
-		{
-			return attentuation * color(scattered, world, depth + 1);
-		} else {
-			return Vec3(0.f, 0.f, 0.f);
+		Vec3 attenuation;
+		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+			return attenuation * color(scattered, world, depth + 1);
 		}
-	} else {
+		else {
+			return Vec3(0, 0, 0);
+		}
+	}
+	else {
 		Vec3 unit_direction = unit_vector(r.direction());
 		float t = 0.5f * (unit_direction.y() + 1.0f);
 		return (1.0f - t) * Vec3(1.0f, 1.0f, 1.0f) + t * Vec3(0.5f, 0.7f, 1.0f);
 	}
 }
 
-int main()
+//Raytraces entire screen
+void RayTrace(int nx, int ny, Shape* world, CImg<unsigned char>& img, Camera& cam)
 {
-	int nx, ny, countRows;
-	nx = 500;
-	ny = 250;
-
-	CImg<unsigned char> img(nx, ny, 1, 3);
-	CImgDisplay canvas(img, "RayTracing Test", 0);
-	
-	Vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
-	Vec3 horizontal(4.0f, 0.0f, 0.0f);
-	Vec3 vertical(0.0f, 2.0f, 0.0f);
-	Vec3 origin(0.0f, 0.0f, 0.0f);
-
-	Shape* list[2];
-	list[0] = new Sphere(Vec3(0, 0, -1), 0.5f, new Lambertian(Vec3(0.8f, 0.3f, 0.3f)));
-	list[1] = new Sphere(Vec3(0, -100.5, -1), 100, new Lambertian(Vec3(0.8f, 0.8f, 0.f)));
-	//list[2] = new Sphere(Vec3(1, 0, -1), 100, new Metal(Vec3(0.8f, 0.8f, 0.f), 0.f));
-	//list[3] = new Sphere(Vec3(-1, 0, -1), 100, new Metal(Vec3(0.8f, 0.8f, 0.f), 0.f));
-	Shape* world = new Shape_List(list, 2);
-
-	Camera cam;
-
-	countRows = 0;
+	int countRows = 0;
 	for (int j = ny - 1; j >= 0; j--, countRows++) {
 		for (int i = 0; i < nx; i++) {
 			Vec3 col(0, 0, 0);
@@ -69,6 +52,7 @@ int main()
 				col += color(r, world);
 			}
 			col /= float(max_samples);
+			col = Vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
 			int ir = int(255.99f * col[0]);
 			int ig = int(255.99f * col[1]);
@@ -81,12 +65,113 @@ int main()
 		float percentDone = 100.f * (float(countRows) / float(ny - 1));
 		cout << percentDone << "%" << endl;
 	}
-	canvas.resize(nx, ny);
-	img.mirror('y');
-	while (!canvas.is_closed() && !canvas.is_keyESC()) {
-		img.display(canvas);
-		cimg::wait(20);
+}
+
+//Raytraces a specific area of the screen
+void RayTraceArea(int nxMin, int nyMin, int nxMax, int nyMax, int nxActual, int nyActual, Shape* world, CImg<unsigned char>& img, Camera& cam)
+{
+	int countRows = 0;
+	for (int j = (nyMax)-1; j >= nyMin; j--, countRows++) {
+		for (int i = nxMin; i < (nxMax); i++) {
+			Vec3 col(0, 0, 0);
+			for (int sample = 0; sample < max_samples; sample++)
+			{
+				float u = float(i + random_double()) / float(nxActual);
+				float v = float(j + random_double()) / float(nyActual);
+				Ray r = cam.get_ray(u, v);
+				col += color(r, world);
+			}
+			col /= float(max_samples);
+			col = Vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+
+			int ir = int(255.99f * col[0]);
+			int ig = int(255.99f * col[1]);
+			int ib = int(255.99f * col[2]);
+
+			img(i, j, 0, 0) = ir;
+			img(i, j, 0, 1) = ig;
+			img(i, j, 0, 2) = ib;
+		}
+		float percentDone = 100.f * (float(countRows) / float((nyMax - nyMin) - 1));
+		cout << percentDone << "%" << endl;
 	}
+}
+
+//Speeds up raytracing by segmenting the tracing into different threads that happen in parallel
+void RayTraceFaster(int power, int nx, int ny, Shape* world, CImg<unsigned char>& img, Camera& cam, CImgDisplay& canvas)
+{
+	std::vector<std::thread> threads;
+
+	if (power <= 0)
+	{
+		throw std::runtime_error("Get outta here with your negative powers and divide by zero shit");
+	}
+
+
+	{
+		for (int i = 0; i < power; i++)
+		{
+			for (int j = 0; j < power; j++)
+			{
+				RayTraceArea((j * nx) / power, (i * ny) / power, ((j + 1) * nx) / power, ((i + 1) * ny) / power, nx, ny, world, img, cam);
+				canvas.display(img);
+			}
+		}
+	}
+
+	////Join threads at end
+	//for (int i = 0; i < threads.size(); i++)
+	//{
+	//	threads[i].join();
+	//}
+}
+
+int main()
+{
+	try {
+		int nx, ny, countRows;
+		nx = 500;
+		ny = 250;
+
+		CImg<unsigned char> img(nx, ny, 1, 3);
+		CImgDisplay canvas(img, "RayTracing Test", 0);
+
+		Vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
+		Vec3 horizontal(4.0f, 0.0f, 0.0f);
+		Vec3 vertical(0.0f, 2.0f, 0.0f);
+		Vec3 origin(0.0f, 0.0f, 0.0f);
+
+		Shape* list[5];
+		list[0] = new Sphere(Vec3(0, 0, -1), 0.5, new Lambertian(Vec3(0.1, 0.2, 0.5)));
+		list[1] = new Sphere(Vec3(0, -100.5, -1), 100, new Lambertian(Vec3(0.8, 0.8, 0.0)));
+		list[2] = new Sphere(Vec3(1, 0, -1), 0.5, new Metal(Vec3(0.8, 0.6, 0.2), 0.f));
+		list[3] = new Sphere(Vec3(-1, 0, -1), 0.5, new Dielectric(1.5));
+		list[4] = new Sphere(Vec3(-1, 0, -1), -0.45, new Dielectric(1.5));
+		Shape* world = new Shape_List(list, 5);
+
+		Vec3 lookfrom(3, 3, 2);
+		Vec3 lookat(0, 0, -1);
+		float dist_to_focus = (lookfrom - lookat).length();
+		float aperture = 0.2;
+
+		Camera cam(lookfrom, lookat, Vec3(0, 1, 0), 20.f,
+			float(nx) / float(ny), aperture, dist_to_focus);
+
+
+		RayTraceFaster(0, nx, ny, world, img, cam, canvas);
+
+		canvas.resize(nx, ny);
+		img.mirror('y');
+		while (!canvas.is_closed() && !canvas.is_keyESC()) {
+			img.display(canvas);
+			cimg::wait(20);
+		}
+	}
+	catch (exception & e) {
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
 }
 
 
--- RayTracingWorks/RayTracingWorks/utils.h
+++ RayTracingWorks/RayTracingWorks/utils.h
@@ -25,8 +25,27 @@ Vec3 random_in_unit_sphere()
 	return p;
 }
 
-Vec3 reflect(const Vec3& v, const Vec3& n)
+Vec3 reflect(const Vec3& v, const Vec3& n) 
 {
 	return v - 2 * dot(v, n) * n;
 }
 
+bool refract(const Vec3& v, const Vec3& n, float ni_over_nt, Vec3& refracted) 
+{
+	Vec3 uv = unit_vector(v);
+	float dt = dot(uv, n);
+	float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt * dt);
+	if (discriminant > 0) {
+		refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+float schlick(float cosine, float ref_idx) 
+{
+	float r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0 * r0;
+	return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
